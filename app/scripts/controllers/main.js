7980d9027cd88fe467be780a4763d0907fdefe77
Matthias Pfeil <matthias.pfeil@wwu.de>
2014-06-06 19:46:57 +0100

    added archive function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,44 @@
 'use strict';
 
 angular.module('lxlfApp')
-  .controller('MainCtrl', ['$scope',
-    function($scope) {
+  .controller('MainCtrl', ['$scope', 'lxlfFactory',
+    function($scope, lxlfService) {
+      
+      $scope.found = 0;
+      $scope.lost = 0;
+      $scope.happy = 0;
 
+      function checkStatus(entry) {
+        var category = entry.snapshot.value.category;
+        var flagged = entry.snapshot.value.flag;
+        if (!flagged) {
+          switch(category) {
+            case 'lost':
+              $scope.lost++;
+              break;
+            case 'found':
+              $scope.found++;
+              break;
+          }
+        } else {
+          $scope.happy++;
+          switch(category) {
+            case 'lost':
+              if ($scope.lost !== 0) {$scope.lost--;}
+              break;
+            case 'found':
+              if ($scope.found !== 0) {$scope.found--;}
+              break;
+          }
+        }
+      }
+
+      lxlfService.$on('child_added', function(entry){
+        checkStatus(entry);
+      });
+
+      lxlfService.$on('child_changed', function(entry){
+        checkStatus(entry);
+      });
     }
   ]);
--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -4,21 +4,21 @@ angular.module('lxlfApp')
   .controller('MapCtrl', ['$scope', 'lxlfFactory', 'leafletData',
     function($scope, lxlfService, leafletData) {
       var drawnItems = new L.FeatureGroup(),
-          options = {
-            edit: {
-              featureGroup: drawnItems,
-              remove: false,
-              edit: false
-            },
-            draw: {
-              polyline: false,
-              polygon: false,
-              circle: false,
-              rectangle: false,
-
-            }
+        options = {
+          edit: {
+            featureGroup: drawnItems,
+            remove: false,
+            edit: false
           },
-          drawControl = new L.Control.Draw(options);
+          draw: {
+            polyline: false,
+            polygon: false,
+            circle: false,
+            rectangle: false,
+
+          }
+        },
+        drawControl = new L.Control.Draw(options);
 
       L.drawLocal.draw.toolbar.buttons.marker = 'Add new Lost & Found';
       L.drawLocal.draw.handlers.marker.tooltip.start = 'Click to place marker!';
@@ -26,19 +26,19 @@ angular.module('lxlfApp')
       var filterControl = L.control();
       filterControl.setPosition('topleft');
       filterControl.onAdd = function () {
-          var className = 'leaflet-control-my-location',
-              container = L.DomUtil.create('div', className + ' leaflet-bar leaflet-control');
-          var link = L.DomUtil.create('a', ' ', container);
-          link.href = '#';
-          L.DomUtil.create('i','fa fa-list fa-lg', link);
+        var className = 'leaflet-control-my-location',
+            container = L.DomUtil.create('div', className + ' leaflet-bar leaflet-control');
+        var link = L.DomUtil.create('a', ' ', container);
+        link.href = '#';
+        L.DomUtil.create('i','fa fa-list fa-lg', link);
 
-          L.DomEvent
-            .on(link, 'click', L.DomEvent.preventDefault)
-            .on(link, 'click', function(){
-              $scope.filter = true;
-            });
+        L.DomEvent
+          .on(link, 'click', L.DomEvent.preventDefault)
+          .on(link, 'click', function(){
+            $scope.filter = true;
+          });
 
-          return container;
+        return container;
       };
 
       $scope.controls = {
@@ -47,6 +47,23 @@ angular.module('lxlfApp')
 
       $scope.filter = false;
 
+      function createUUID() {
+        // http://www.ietf.org/rfc/rfc4122.txt
+        var s = [];
+        var hexDigits = '0123456789abcdef';
+        for (var i = 0; i < 36; i++) {
+          s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = '4';  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = '-';
+
+        var uuid = s.join('');
+        return uuid;
+      }
+
+      $scope.securitycode = '';
+
       leafletData.getMap().then(function(map){
         map.addLayer(drawnItems);
 
@@ -54,6 +71,7 @@ angular.module('lxlfApp')
           var layer = e.layer;
           $scope.newLFMarker.lat = layer._latlng.lat;
           $scope.newLFMarker.lng = layer._latlng.lng;
+          $scope.securitycode = createUUID();
           layer.on('dragend', function(e){
             $scope.newLFMarker.lat = e.target._latlng.lat;
             $scope.newLFMarker.lng = e.target._latlng.lng;
@@ -77,7 +95,7 @@ angular.module('lxlfApp')
         lng: 7,
       };
 
-      $scope.cancelForm = function() {
+      function clearNewLFForm() {
         $scope.newLF = false;
         $scope.newItem.category = '';
         $scope.newItem.short = '';
@@ -89,6 +107,10 @@ angular.module('lxlfApp')
         leafletData.getMap().then(function(map){
           drawnItems.clearLayers();
         });
+      }
+
+      $scope.cancelForm = function() {
+        clearNewLFForm();
       };
 
       // function to submit the form after all validation has occurred      
@@ -106,9 +128,11 @@ angular.module('lxlfApp')
             date: $scope.newItem.date,
             tags: $scope.newItem.tags,
             category: $scope.newItem.category,
-            contact: $scope.newItem.contact
+            contact: $scope.newItem.contact,
+            sec: $scope.securitycode
           });
           $scope.newLF = false;
+          clearNewLFForm();
         }
       };
 
@@ -124,6 +148,28 @@ angular.module('lxlfApp')
         startingDay: 1
       };
 
+      $scope.archiveIsCollapsed = true;
+      $scope.secName = [];
+      $scope.archive = function(marker,sec) {
+        var secObj = {};
+        secObj.name = marker.title;
+        secObj.sec = sec;
+        var ref = new Firebase('https://lxlf.firebaseio.com/lost/'+marker.title);
+        for (var i = $scope.secName.length - 1; i >= 0; i--) {
+          if ($scope.secName[i].name === marker.title && $scope.secName[i].sec === sec) {
+            ref.update({flag:1}, function(error){
+              if (error) {
+                //TODO error
+              } else {
+                $scope.selected = false;
+              }
+            });
+          } else {
+            //TODO error
+          }
+        }
+      };
+
       $scope.initDate = new Date('2016-15-20');
       $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
       $scope.format = $scope.formats[0];
@@ -134,45 +180,61 @@ angular.module('lxlfApp')
       };
 
       lxlfService.$on('child_added', function(entry){
+        console.log('child_added');
+        var secName = {};
         var marker = {};
-        marker.lat = entry.snapshot.value.lat;
-        marker.lng = entry.snapshot.value.lng;
-        marker.title = entry.snapshot.name;
-        marker.date = entry.snapshot.value.date;
-        marker.desc = entry.snapshot.value.desc;
-        marker.descLong = entry.snapshot.value.descLong;
-        marker.contact = entry.snapshot.value.contact;
-        marker.comments = entry.snapshot.value.comments;
-        marker.category = entry.snapshot.value.category;
-        marker.tags = entry.snapshot.value.tags.map(function(elem){
-          return elem.text;
-        }).join(", ");
-        $scope.markers.push(marker);
+        if (!entry.snapshot.value.flag) {
+          secName.name = entry.snapshot.name;
+          secName.sec = entry.snapshot.value.sec;
+          marker.lat = entry.snapshot.value.lat;
+          marker.lng = entry.snapshot.value.lng;
+          marker.title = entry.snapshot.name;
+          marker.date = entry.snapshot.value.date;
+          marker.desc = entry.snapshot.value.desc;
+          marker.descLong = entry.snapshot.value.descLong;
+          marker.contact = entry.snapshot.value.contact;
+          marker.comments = entry.snapshot.value.comments;
+          marker.category = entry.snapshot.value.category;
+          marker.tags = entry.snapshot.value.tags.map(function(elem){
+            return elem.text;
+          }).join(', ');
+          $scope.markers.push(marker);
+          $scope.secName.push(secName);
+        }
       });
 
       lxlfService.$on('child_changed', function(entry){
-        console.log('child changed');
-        console.log(entry);
-        var marker = {};
-        for (var i = $scope.markers.length - 1; i >= 0; i--) {
-          if ($scope.markers[i].title === entry.snapshot.name) {
-            marker.lat = entry.snapshot.value.lat;
-            marker.lng = entry.snapshot.value.lng;
-            marker.title = entry.snapshot.name;
-            marker.date = entry.snapshot.value.date;
-            marker.desc = entry.snapshot.value.desc;
-            marker.descLong = entry.snapshot.value.descLong;
-            marker.contact = entry.snapshot.value.contact;
-            marker.comments = entry.snapshot.value.comments;
-            marker.tags = entry.snapshot.value.tagsmap(function(elem){
-              return elem.text;
-            }).join(",");
-            $scope.markers[i] = marker;
-            break;
+        console.log('child_changed');
+        if (entry.snapshot.value.flag) {
+          for (var i = $scope.markers.length - 1; i >= 0; i--) {
+            if ($scope.markers[i].title === entry.snapshot.name) {
+              $scope.markers.splice(i,1);
+              break;
+            }
           }
-        }
-        if ($scope.selectedMarker.title === entry.snapshot.name) {
-          $scope.selectedMarker = marker;
+          $scope.selectedMarker.length = 0;
+        } else {
+          var marker = {};
+          for (var i = $scope.markers.length - 1; i >= 0; i--) {
+            if ($scope.markers[i].title === entry.snapshot.name) {
+              marker.lat = entry.snapshot.value.lat;
+              marker.lng = entry.snapshot.value.lng;
+              marker.title = entry.snapshot.name;
+              marker.date = entry.snapshot.value.date;
+              marker.desc = entry.snapshot.value.desc;
+              marker.descLong = entry.snapshot.value.descLong;
+              marker.contact = entry.snapshot.value.contact;
+              marker.comments = entry.snapshot.value.comments;
+              marker.tags = entry.snapshot.value.tagsmap(function(elem){
+                return elem.text;
+              }).join(',');
+              $scope.markers[i] = marker;
+              break;
+            }
+          }
+          if ($scope.selectedMarker.title === entry.snapshot.name) {
+            $scope.selectedMarker = marker;
+          }
         }
       });
 
